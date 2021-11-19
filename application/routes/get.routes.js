2e0e640b9034fa9129c6cbb103ac317ded319496
mohitpatidar16 <91652873+mohitpatidar16@users.noreply.github.com>
2021-11-19 19:27:57 +0530

    eiuweto;

--- application/routes/get.routes.js
+++ application/routes/get.routes.js
@@ -6,6 +6,9 @@ const authenticate = require("../middleware/authenticate");
 router.get("/", (req, res) => {
  res.render("index.hbs");
   });
+  router.get("/:id/attendance", (req, res) => {
+    res.render("attendance");
+     });
  
   router.get("/about", authenticate, (req, res) => {
     console.log("hello i am about");
