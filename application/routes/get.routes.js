c09de650745932e0fbdd09566791279a8aa5aba6
VASUDEV KANUNGO <77634422+vasu2705@users.noreply.github.com>
2021-11-20 10:51:57 +0530

    Update

--- application/routes/get.routes.js
+++ application/routes/get.routes.js
@@ -2,41 +2,37 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middleware/authenticate");
 
-
 router.get("/", (req, res) => {
- res.render("index.hbs");
+  res.render("index.hbs", {
+    title: "index",
   });
-  router.get("/attendance", (req, res) => {
-    res.render("attendance.hbs");
-     });
-
-     router.get("/auth-forgot-password", (req, res) => {
-      res.render("auth-forgot-password");
-       });
-
-       router.get("/login", (req, res) => {
-        res.render("auth-login");
-         });
-         router.get("/signup", (req, res) => {
-          res.render("auth-register");
-           });
-
-           router.get("/profile", (req, res) => {
-            res.render("features-profile");
-             });
-
-             router.get("/syllabustrack", (req, res) => {
-              res.render("SyllabusTrack");
-               });
- 
-  router.get("/about", authenticate, (req, res) => {
-    console.log("hello i am about");
-    res.send(req.UserRoot);
-
+});
+router.get("/attendance", (req, res) => {
+  res.render("attendance.hbs");
 });
 
+router.get("/auth-forgot-password", (req, res) => {
+  res.render("auth-forgot-password");
+});
 
+router.get("/login", (req, res) => {
+  res.render("auth-login");
+});
+router.get("/signup", (req, res) => {
+  res.render("auth-register");
+});
 
+router.get("/profile", (req, res) => {
+  res.render("features-profile");
+});
 
+router.get("/syllabustrack", (req, res) => {
+  res.render("SyllabusTrack");
+});
 
-  module.exports =router;
+router.get("/about", authenticate, (req, res) => {
+  console.log("hello i am about");
+  res.send(req.UserRoot);
+});
+
+module.exports = router;
